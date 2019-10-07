25c8b2c0d6f91898e1abb65c8993ca5b9328db39
Bert Keller <bertwinkeller@gmail.com>
2019-10-06 22:23:35 -0700

    firebase hooked up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,14 @@
-console.log('hi')
+var Config = {
+    apiKey: "AIzaSyAaeUyb0fm59FeMgLaX5kV2kMQ0tjENDm8",
+    authDomain: "traintracker-53d7b.firebaseapp.com",
+    databaseURL: "https://traintracker-53d7b.firebaseio.com",
+    projectId: "traintracker-53d7b",
+    storageBucket: "traintracker-53d7b.appspot.com",
+    messagingSenderId: "201897892328",
+    appId: "1:201897892328:web:eba5be7362169506122c5d",
+    measurementId: "G-NXRJSGBF5P"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(Config);
+  
+  const db = firebase.database();
