2bf19491f132556af481e4b45b16d27654cded3e
Bert Keller <bertwinkeller@gmail.com>
2019-10-07 18:00:10 -0700

    storing values for text inputs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,3 +12,21 @@ var Config = {
   firebase.initializeApp(Config);
   
   const db = firebase.database();
+
+
+  let name;
+  let destination;
+  let firstTrain;
+  let frequency;
+
+  let addTrain = document.getElementById('addTrain');
+  if(addTrain){
+      addEventListener('click', function (){
+   event.preventDefault();
+   name = document.getElementById('train-name').value;
+   destination = document.getElementById('destination').value;
+   firstTrain = document.getElementById('first-train').value;
+   frequency = document.getElementById('frequency').value;
+   console.log(name,destination,firstTrain,frequency)
+      
+  })};
